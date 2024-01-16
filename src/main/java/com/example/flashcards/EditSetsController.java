7bbd50f3d4e1128aa595e339f12bc5931df9f442
andhot01 <andra.hotnogu@yahoo.com>
2024-01-16 21:35:36 +0100

    New fxml file, class and controller for edit window

---
+++ src/main/java/com/example/flashcards/EditSetsController.java
@@ -0,0 +1,36 @@
+package com.example.flashcards;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class EditSetsController {
+    @FXML
+    private ListView<?> cardsInSet;
+
+    @FXML
+    private Button deleteCardButton, newCardButton, searchCardButton;
+
+    @FXML
+    void deleteCard(MouseEvent event) {
+
+    }
+
+    @FXML
+    void makeNewCard(MouseEvent event) {
+
+    }
+
+    @FXML
+    void searchCard(MouseEvent event) {
+
+    }
+
+    @FXML
+    void selectCard(MouseEvent event) {
+
+    }
+
+
+}
--- src/main/java/com/example/flashcards/FlashController.java
+++ src/main/java/com/example/flashcards/FlashController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 
@@ -19,22 +20,24 @@ public class FlashController {
     private Rectangle pi0, pi1, pi2, pi3, pi4, pi5, pi6, pi7;
 
     private IntroWindow iw = new IntroWindow();
+    private EditWindow ew = new EditWindow();
 
     @FXML
     void editSetName(MouseEvent event) {
-        Label clickedLabel = (Label) event.getSource();
-
+        Label clickedLabel = (Label) event.getSource();//get selected label to avoid repeating same code 8 times
+//Turns label into textField for user input, empties label so previous text doesn't show
         TextField txt = new TextField(clickedLabel.getText());
         clickedLabel.setText(" ");
         clickedLabel.setGraphic(txt);
 
+//when enter is pressed label returns with new text
         txt.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) {
                 clickedLabel.setText(txt.getText());
                 clickedLabel.setGraphic(null);
             }
         });
-
+//make method for db to keep changes
     }
 
     @FXML
@@ -49,6 +52,10 @@ public class FlashController {
 
     @FXML
     void openSet(MouseEvent event) {
+        if (event.getButton() == MouseButton.SECONDARY) {
+            Rectangle clickedRectangle = (Rectangle) event.getSource();//selected rectangle for each set
 
+            ew.makeChange(clickedRectangle); //class for editWindow to save space
+        }
     }
 }
